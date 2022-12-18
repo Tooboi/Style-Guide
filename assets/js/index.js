cdeb5ce85527a9bf667f5d056277eedb09307846
Tooboi <japica123@gmail.com>
2022-12-18 03:31:09 -0500

    forms update on change and populate text fields

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,13 +4,16 @@
   const backgroundInput = document.getElementById("backgroundInput");
   const surfaceInput = document.getElementById("surfaceInput");
   const borderInput = document.getElementById("borderInput");
+
   const p200Input = document.getElementById("p200Input");
   const p500Input = document.getElementById("p500Input");
   const p700Input = document.getElementById("p700Input");
+
   const s200Input = document.getElementById("s200Input");
   const s500Input = document.getElementById("s500Input");
   const s700Input = document.getElementById("s700Input");
-  const onBackgroundInput = document.getElementById("onBackgroundInput");
+
+  // const onBackgroundInput = document.getElementById("onBackgroundInput");
   const onSurfaceInput = document.getElementById("onSurfaceInput");
   const onPrimaryInput = document.getElementById("onPrimaryInput");
   const onSecondaryInput = document.getElementById("onSecondaryInput");
@@ -24,23 +27,28 @@
 
   // Forms
   const formLayout = document.getElementById("formLayout");
+  const formPrimary = document.getElementById("formPrimary");
+  const formSecondary = document.getElementById("formSecondary");
+  const formText = document.getElementById("formText");
   const formRandom = document.getElementById("randomize");
   const formFonts = document.getElementById("formFonts");
 
   // Color swatches
-  const p200Text = document.getElementById("swatch1T");
-  const p500Text = document.getElementById("swatch2T");
-  const p700Text = document.getElementById("swatch3T");
-  const s200Text = document.getElementById("swatch4T");
-  const s500Text = document.getElementById("swatch5T");
-  const s700Text = document.getElementById("swatch6T");
-  const backgroundText = document.getElementById("swatch11T");
-  const surfaceText = document.getElementById("swatch12T");
-  const borderText = document.getElementById("swatch13T");
-  const onBackgroundText = document.getElementById("swatch7T");
-  const onSurfaceText = document.getElementById("swatch8T");
-  const onPrimaryText = document.getElementById("swatch9T");
-  const onSecondaryText = document.getElementById("swatch10T");
+  const p200Text = document.getElementById("p200Text");
+  const p500Text = document.getElementById("p500Text");
+  const p700Text = document.getElementById("p700Text");
+
+  const s200Text = document.getElementById("s200Text");
+  const s500Text = document.getElementById("s500Text");
+  const s700Text = document.getElementById("s700Text");
+
+  const backgroundText = document.getElementById("bgText");
+  const surfaceText = document.getElementById("surfText");
+  const borderText = document.getElementById("borText");
+
+  const onSurfaceText = document.getElementById("onStext");
+  const onPrimaryText = document.getElementById("onPtext");
+  const onSecondaryText = document.getElementById("onSecText");
 
   // Font inputs
   // const crimsonPro = document.getElementById("crimsonPro");
@@ -59,54 +67,62 @@
   const schemes = [schAnalogous, schComplement, schTriad, schTetrad]
   const fonts = [fraunces, montserrat, nunito, poppins, quicksand, roboto];
 
+  formLayout.addEventListener("input", () => {
+      const background = backgroundInput.value;
+      const surface = surfaceInput.value;
+      const border = borderInput.value;
 
+      rootTheme.style.setProperty("--col-bg", background);
+      rootTheme.style.setProperty("--col-surface", surface);
+      rootTheme.style.setProperty("--col-surface2", border);
 
-  formLayout.addEventListener("change", (event) => {
-    event.preventDefault();
-    const background = backgroundInput.value;
-    const surface = surfaceInput.value;
-    const border = borderInput.value;
-    const p200 = p200Input.value;
-    const p500 = p500Input.value;
-    const p700 = p700Input.value;
-    const s200 = s200Input.value;
-    const s500 = s500Input.value;
-    const s700 = s700Input.value;
-    const onBackground = onBackgroundInput.value;
-    const onSurface = onSurfaceInput.value;
-    const onPrimary = onPrimaryInput.value;
-    const onSecondary = onSecondaryInput.value;
+      backgroundText.innerText = background;
+      surfaceText.innerText = surface;
+      borderText.innerText = border;
+    })
 
-    rootTheme.style.setProperty("--col-bg", background);
-    rootTheme.style.setProperty("--col-surface", surface);
-    rootTheme.style.setProperty("--col-surface2", border);
-    rootTheme.style.setProperty("--col-p-200", p200);
-    rootTheme.style.setProperty("--col-p-500", p500);
-    rootTheme.style.setProperty("--col-p-700", p700);
-    rootTheme.style.setProperty("--col-s-200", s200);
-    rootTheme.style.setProperty("--col-s-500", s500);
-    rootTheme.style.setProperty("--col-s-700", s700);
-    rootTheme.style.setProperty("--col-on-bg", onBackground);
-    rootTheme.style.setProperty("--col-on-surface", onSurface);
-    rootTheme.style.setProperty("--col-on-primary", onPrimary);
-    rootTheme.style.setProperty("--col-on-secondary", onSecondary);
+    formPrimary.addEventListener("input", () => {
+      const p200 = p200Input.value;
+      const p500 = p500Input.value;
+      const p700 = p700Input.value;
 
-    p200Text.innerText = p200;
-    p500Text.innerText = p500;
-    p700Text.innerText = p700;
-    s200Text.innerText = s200;
-    s500Text.innerText = s500;
-    s700Text.innerText = s700;
-    backgroundText.innerText = background;
-    surfaceText.innerText = surface;
-    borderText.innerText = border;
-    onBackgroundText.innerText = onBackground;
-    onSurfaceText.innerText = onSurface;
-    onPrimaryText.innerText = onPrimary;
-    onSecondaryText.innerText = onSecondary;
+      rootTheme.style.setProperty("--col-p-200", p200);
+      rootTheme.style.setProperty("--col-p-500", p500);
+      rootTheme.style.setProperty("--col-p-700", p700);
 
-    
-  });
+      p200Text.innerText = p200;
+      p500Text.innerText = p500;
+      p700Text.innerText = p700;
+    })
+
+    formSecondary.addEventListener("input", () => {
+      const s200 = s200Input.value;
+      const s500 = s500Input.value;
+      const s700 = s700Input.value;
+
+      rootTheme.style.setProperty("--col-s-200", s200);
+      rootTheme.style.setProperty("--col-s-500", s500);
+      rootTheme.style.setProperty("--col-s-700", s700);
+
+      s200Text.innerText = s200;
+      s500Text.innerText = s500;
+      s700Text.innerText = s700;
+    })
+
+    formText.addEventListener("input", () => {
+      const onSurface = onSurfaceInput.value;
+      const onPrimary = onPrimaryInput.value;
+      const onSecondary = onSecondaryInput.value;
+
+      rootTheme.style.setProperty("--col-on-surface", onSurface);
+      rootTheme.style.setProperty("--col-on-primary", onPrimary);
+      rootTheme.style.setProperty("--col-on-secondary", onSecondary);
+
+      onSurfaceText.innerText = onSurface;
+      onPrimaryText.innerText = onPrimary;
+      onSecondaryText.innerText = onSecondary;
+    })
+
 
 
   var primaryCol = "";
@@ -198,21 +214,21 @@
     rootTheme.style.setProperty("--col-s-200", secondaryLight);
     rootTheme.style.setProperty("--col-s-500", secondaryCol);
     rootTheme.style.setProperty("--col-s-700", secondaryDark);
-    // rootTheme.style.setProperty("--col-on-bg", onBackground);
+
     // rootTheme.style.setProperty("--col-on-surface", onSurface);
     // rootTheme.style.setProperty("--col-on-primary", onPrimary);
     // rootTheme.style.setProperty("--col-on-secondary", onSecondary);
 
-    // p200Text.innerText = primaryLight;
-    // p500Text.innerText = primaryColConfirm;
-    // p700Text.innerText = primaryDark;
-    // s200Text.innerText = secondaryLight;
-    // s500Text.innerText = secondaryCol;
-    // s700Text.innerText = secondaryDark;
-    // backgroundText.innerText = backgroundCol;
-    // surfaceText.innerText = surfaceCol;
-    // borderText.innerText = borderCol;
-    // onBackgroundText.innerText = onBackground;
+    p200Text.innerText = primaryLight;
+    p500Text.innerText = primaryColConfirm;
+    p700Text.innerText = primaryDark;
+    s200Text.innerText = secondaryLight;
+    s500Text.innerText = secondaryCol;
+    s700Text.innerText = secondaryDark;
+    backgroundText.innerText = backgroundCol;
+    surfaceText.innerText = surfaceCol;
+    borderText.innerText = borderCol;
+
     // onSurfaceText.innerText = onSurface;
     // onPrimaryText.innerText = onPrimary;
     // onSecondaryText.innerText = onSecondary;
